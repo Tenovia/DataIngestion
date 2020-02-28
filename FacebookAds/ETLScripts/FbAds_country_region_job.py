ae19aca5110f38d6f87a5d6defd79bb39ec92c8d
Booyah1898 <sanjkrao@gmail.com>
2020-02-28 16:02:22 +0530

    Update FbAds_country_region_job.py

--- FacebookAds/ETLScripts/FbAds_country_region_job.py
+++ FacebookAds/ETLScripts/FbAds_country_region_job.py
@@ -15,8 +15,11 @@ for x in range(1,8):
     arr=date1.split("-")
     year=arr[0]
     month=arr[1]
-    data_file ="s3://paragon-datalake/FacebookAds/landing_country_region/"+"country_region_"+date1+".csv"
+    try:
+        data_file ="s3://paragon-datalake/FacebookAds/landing_country_region/"+"country_region_"+date1+".csv"
 
-    df= spark.read.csv(data_file, header=True,inferSchema=True, sep=",").cache()
-    
-    df.write.mode('overwrite').parquet("s3://paragon-datalake/FacebookAds/processing_country_region/parquet/"+year+"/"+month+"/"+"country_region_"+date1)
+        df= spark.read.csv(data_file, header=True,inferSchema=True, sep=",").cache()
+
+        df.write.mode('overwrite').parquet("s3://paragon-datalake/FacebookAds/processing_country_region/parquet/"+year+"/"+month+"/"+"country_region_"+date1)
+    except:
+        continue
