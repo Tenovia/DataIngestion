6bd0f57c9bfbe0222ea29ae885f554f1c99d96bd
akhil1010 <31964521+akhil1010@users.noreply.github.com>
2020-02-28 15:37:57 +0530

    Create cred.py

---
+++ AmazonSellerCentral/cred.py
@@ -0,0 +1,19 @@
+def marketplaceid():								#for this we have to get marketplace ID
+	"""Function to return MarketPlaceId"""
+	MarketPlaceID = 'A21TJRUUN4KGV'
+	return MarketPlaceID
+
+def merchantid():								#Merchant ID, it is given by developer account
+	"""Function to return MerchantId"""	
+	MerchantID = 'XXXXXXXXXXXX'
+	return MerchantID
+
+def accessid():									#AccessID, it is given by developer account
+	"""Function to return AccessId"""
+	AccessKeyID = 'XXXXXXXXXXXXXXXXXXXXX' 
+	return AccessKeyID
+
+def secretkey():								#SecretKey, it is given by developer account
+	"""Function to return SecretKey"""
+	SecretKey = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+	return SecretKey
